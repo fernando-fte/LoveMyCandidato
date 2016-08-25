daf5a8e4c2ed9aa321abcfa73e557883d9cb6c3f
fernando-fte <fernando.fte@gmail.com>
2016-08-24 21:18:57 -0300

    Olá mundo

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Love my Candidato</title>
+
+		<!-- VENDOR: Boostatrap CSS -->
+		<link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+
+		<!-- VENDOR: Boostatrap FontAwesome -->
+		<link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap/css/font-awesome.min.css">
+	</head>
+
+	<body>
+
+		<div class="container">
+			<h1>Bem vindo</h1>
+		</div>
+
+		<!-- VENDOR: Jquery Javscript -->
+		<script type="text/javascript" src="assets/vendor/scripts/jquery.min.js"></script>
+
+		<!-- VENDOR: Boostatrap script -->
+		<script type="text/javascript" src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+		<!-- VENDOR: Coffee -->
+		<script type="text/javascript" src="assets/vendor/scripts/coffee-script.js"></script>
+
+		<!-- VENDOR: Less 
+		<script type="text/javascript" src="assets/vendor/scripts/less.min.js"></script>
+		-->
+	</body>
+</html>
